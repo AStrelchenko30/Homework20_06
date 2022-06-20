31c0d7fe4d45f0fce30b385ee46be279e5168a70
V-I-K-I <anystrelchenko@gmail.com>
2022-06-20 18:15:26 +0300

    Initial commit

---
+++ src/main/java/com/example/homework20_06/Homework2006Application.java
@@ -0,0 +1,13 @@
+package com.example.homework20_06;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework2006Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework2006Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/homework20_06/Homework2006ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homework20_06;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework2006ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
